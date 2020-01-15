c89abf04ecb71352b4e39bc2c01a3f9ef44bd14e
vishwajeetiitb <vishwajeet724728@gmail.com>
2020-01-15 19:35:09 +0530

    knob added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 var state = "on";
-function myFunction() {
-    if(state=="on"){state="off"}
-   else if(state=="off"){state="on"}
-    document.getElementById("demo").innerHTML = state;
-    testref.set({test: state}) ; 
+function nightled() {
+  if (state == "on") {
+    state = "off";
+  } else if (state == "off") {
+    state = "on";
   }
+  document.getElementById("demo").innerHTML = state;
+  testref.set(state);
+}
+function fan(speed) {
+  console.log(speed);
+}
